b0d421adcc96c71029c5949873fe4b36cbf54484
Meghwin-Dave <91586930+Meghwin-Dave@users.noreply.github.com>
2023-08-23 15:03:57 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+  navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active');
+};
+
+var swiper = new Swiper(".products-slider", {
+  loop: true,
+  grabCursor : true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+    850: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper = new Swiper(".arrivals-slider", {
+  loop: true,
+  grabCursor : true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+    850: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
